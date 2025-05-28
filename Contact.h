7aaae9378ef2f491b93d1ef1cf6c752068560d64
WenDaoYolo <3547474131@qq.com>
2025-05-28 22:54:06 +0800

    Add files via upload

--- Contact.h
+++ Contact.h
@@ -1,31 +1,37 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
-#define MAX_NAME 20
-#define MAX_SEX 5
-#define MAX_TELE 12
-#define MAX_ADDR 25
-#define MAX_PEO 1000
+#include<string.h>
+#define DEFAULT 3                           //通讯录初始容量
+#define INC_DEF 2                           //通讯录容量增长单位
+#define LENGTH 77                           //通讯录边框长度
 
-typedef struct PeoInfo {            //构造个人信息类
-	char name[MAX_NAME];
-	char sex[MAX_SEX];
+typedef struct PeoInfo {                    //构造联系人信息类
+	char name[20];
+	char sex[5];
 	int age;
-	char tele[MAX_TELE];
-	char addr[MAX_ADDR];
+	char tele[12];
+	char addr[22];
 }PeoInfo;
 
-typedef struct Contact {            //构造通讯录类  
-	PeoInfo PeoInfo_1[MAX_PEO];
-	int count;                      //记录当前通讯录的有效元素个数
+typedef struct Contact {                     //构造通讯录类
+	int count;
+	int capacity;
+	PeoInfo* PeopleInfomation;
 }Contact;
 
-void PrintIndexMenu();              //打印主菜单
-void initContact(Contact* pcon);    //初始化通讯录
-void AddContact(Contact* pcon);     //添加联系人
-void DisplayContact(Contact* pcon); //显示通讯录信息
-void DeleteContact(Contact* pcon);  //删除联系人
-void ModifyContact(Contact* pcon);  //修改联系人
-void SearchContact(Contact* pcon);  //搜索联系人
-void SortContact(Contact* pcon);    //排序联系人
+void InitContact(Contact* con,char* fn);      //初始化通讯录
+void Destroy(Contact* con,char* fn);          //销毁通讯录
+
+void DisplayIndexMenu();                      //打印主菜单
+void CheckCapacity(Contact* con);             //通讯录增容检查
+
+void AddContact(Contact* con);                //添加联系人
+void DeleteContact(Contact* con);             //删除联系人
+void SearchContact(Contact* con);             //查找联系人
+void ModifyContact(Contact* con);             //修改联系人
+void SortContact(Contact* con);               //排序通讯录
+
+void DisplayContact(Contact* con);            //查看通讯录
+
+
 
